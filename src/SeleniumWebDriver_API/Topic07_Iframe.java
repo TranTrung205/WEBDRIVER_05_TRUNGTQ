2bc54e5edb9cea4477c74b24f8e9957a65dd0858
Trung <Trung@192.168.0.8>
2018-08-17 21:37:43 +0700

    Commit Topic07_TC03_Iframe_WindowHandle

--- src/SeleniumWebDriver_API/Topic07_Iframe.java
+++ src/SeleniumWebDriver_API/Topic07_Iframe.java
@@ -25,8 +25,8 @@ public class Topic07_Iframe {
 		driver.manage().window().maximize();
 
 	}
-	
-	//Iframe Handle
+
+	// Iframe Handle
 	@Test(enabled = false)
 	public void TestScript01() {
 		driver.get("https://www.hdfcbank.com/");
@@ -73,7 +73,7 @@ public class Topic07_Iframe {
 	}
 
 	// Windows Handle
-	@Test
+	@Test(enabled = false)
 	public void TestScript02() {
 		driver.get("http://daominhdam.890m.com/");
 		String parentID = driver.getWindowHandle();
@@ -81,9 +81,13 @@ public class Topic07_Iframe {
 
 		Set<String> allWindows = driver.getWindowHandles();
 		for (String runWindow : allWindows) {
+			// Swwitch vào từng Window
 			driver.switchTo().window(runWindow);
+			// getTitle đã switch wa
 			String currentWindow = driver.getTitle();
-			if(currentWindow.equals("Google")) {
+			// kiểm tra nếu title bằng với expected title truyền vào
+			if (currentWindow.equals("Google")) {
+				// thoát khỏi vòng lặp
 				break;
 			}
 		}
@@ -94,8 +98,77 @@ public class Topic07_Iframe {
 
 	}
 
-	@AfterClass
-	public void afterClass() {
+	@Test
+	public void TestScript03() {
+		// Step 01 - Truy cập vào trang: http://www.hdfcbank.com/
+		driver.get("https://www.hdfcbank.com/");
+		String parentWindow = driver.getWindowHandle();
+
+		// Step 02 - Kiểm tra và close quảng cáo nếu có xuất hiện
+		overrideGlobalWait(10);
+		List<WebElement> popupMessageframe = driver
+				.findElements(By.xpath("//iframe[@id='vizury-notification-template']"));
+		if (popupMessageframe.size() > 0) {
+			driver.switchTo().frame(popupMessageframe.get(0));
+			WebElement closePopUpBtn = driver.findElement(By.xpath("//div[@id='div-close']"));
+			JavascriptExecutor je = (JavascriptExecutor) driver;
+			je.executeScript("arguments[0].click();", closePopUpBtn);
+			driver.switchTo().defaultContent();
+		}
+
+		// Step 03 - Click Angri link -> Mở ra tab/window mới -> Switch qua tab mới
+		overrideGlobalWait(30);
+		driver.findElement(By.xpath("//div[@class='sectionnav']//a[contains(text(),'Agri')]")).click();
+		switchToWindowByTitle("HDFC Bank Kisan Dhan Vikas e-Kendra");
+		
+		//Step 04 - Click Account Details link -> Mở ra tab/window mới -> Switch qua tab mới 
+		driver.findElement(By.xpath("//p[text()='Account Details']")).click();
+		switchToWindowByTitle("Welcome to HDFC Bank NetBanking");
+		
+		//Step 05- Click Privacy Policy link (nằm trong frame) -> Mở ra tab/window mới -> Switch qua tab mới
+		WebElement footerFrame = driver.findElement(By.xpath("//frame[@name='footer']"));
+		driver.switchTo().frame(footerFrame);
+		driver.findElement(By.xpath("//a[text()='Privacy Policy']")).click();
+		switchToWindowByTitle("HDFC Bank - Leading Bank in India, Banking Services, Private Banking, Personal Loan, Car Loan");
+		
+		//Step 06- Click CSR link on Privacy Policy page
+		driver.findElement(By.xpath("//a[text()='CSR']")).click();
+		closeAllWithoutParentWindows(parentWindow);
+
 	}
 
+	@AfterClass
+	public void afterClass() {
+		driver.close();
+	}
+
+	public void switchToWindowByTitle(String title) {
+		Set<String> allWindows = driver.getWindowHandles();
+		for (String runWindows : allWindows) {
+			driver.switchTo().window(runWindows);
+			String currentWin = driver.getTitle();
+			if (currentWin.equals(title)) {
+				break;
+			}
+		}
+	}
+
+	public boolean closeAllWithoutParentWindows(String parentWindow) {
+		Set<String> allWindows = driver.getWindowHandles();
+		for (String runWindows : allWindows) {
+			if (!runWindows.equals(parentWindow)) {
+				driver.switchTo().window(runWindows);
+				driver.close();
+			}
+		}
+		driver.switchTo().window(parentWindow);
+		if (driver.getWindowHandles().size() == 1)
+			return true;
+		else
+			return false;
+	}
+	
+	public void overrideGlobalWait(long timeout) {
+		driver.manage().timeouts().implicitlyWait(timeout, TimeUnit.SECONDS);
+	}
 }
